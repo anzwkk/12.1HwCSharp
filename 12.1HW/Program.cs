1a38b5303bb2d6da7c06e7a022f1e0c0745fa621
anzwkk <anam89737@gmail.com>
2025-01-29 21:31:46 +0300

    Added number input to calculate Fibonacci number

--- 12.1HW/Program.cs
+++ 12.1HW/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+Console.Write("Введіть номер числа Фібоначчі: ");
+int n = int.Parse(Console.ReadLine());
+
+int result = Fibonacci.FibonacciCalculator(n);
+Console.WriteLine($"Число Фібоначчі номер {n}: {result}");
+
 static class Fibonacci
 {
     public static int FibonacciCalculator(int n)
