b9463cf9b725be532e91eb9824565efe7940ac3d
anzwkk <anam89737@gmail.com>
2025-01-29 21:24:37 +0300

    A recursive method for calculating the Fibonacci number

--- 12.1HW/Program.cs
+++ 12.1HW/Program.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 
 static class Fibonacci
-{ 
+{
+    public static int FibonacciCalculator(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        return FibonacciCalculator(n - 1) + FibonacciCalculator(n - 2);
 
+    }
 }
